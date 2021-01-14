b84838885beb9cc315b09817ff60be86b884d82e
hernan r <hobbit.rodrigiu@gmail.com>
2021-01-14 02:27:43 -0300

    start working on home page nave

--- web/index.php
+++ web/index.php
@@ -1,7 +1,40 @@
+<?php ?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hernan Rodriguez | Web Dev</title>
+    
+</head>
 <?php
-
+include("../header.php");
 ?>
+<body>
+  <div class="container">
+   <h1 class="display-1">Hernan Rodriguez</h1>  
 
-<div>
-  Intro page
-</div>
+   <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Home</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Projects</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </div>
+<?php
+include("../footer.php");
+?>
+</body>
+</html>
